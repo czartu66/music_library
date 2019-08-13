78ac77d99f5e571a2612714fd7c971a6144cd5be
Adam Czartoryjski <a.f@example.com>
2019-08-13 17:07:05 +0200

    added create,update,delete view

--- music/models.py
+++ music/models.py
@@ -17,7 +17,7 @@ class Album(models.Model):
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=100)
-    song_title= models.CharField(max_length=250)
+    song_title = models.CharField(max_length=250)
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
--- music/views.py
+++ music/views.py
@@ -1,10 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Album
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
-    # context_object_name = '' - changing default name 'object list'
+    context_object_name = 'all_albums'
 
     def get_queryset(self):
         return Album.objects.all()
@@ -17,3 +18,12 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
