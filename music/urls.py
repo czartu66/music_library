05bc05902eb545483e800055312dfc412698e6d7
Adam Czartoryjski <a.f@example.com>
2019-08-14 17:30:07 +0200

    choose file option

--- music/urls.py
+++ music/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
     # /music/album/2/
-    url(r'album/^(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
 
     # /music/album/2/delete/
     url(r'^(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
